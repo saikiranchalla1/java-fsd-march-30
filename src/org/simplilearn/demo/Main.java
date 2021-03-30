7dd4da46fdc08ad641bc74da16f26d3a30de63b0
schalla <schalla@adobe.com>
2021-03-30 11:42:04 -0600

    Updating the print statement

--- src/org/simplilearn/demo/Main.java
+++ src/org/simplilearn/demo/Main.java
@@ -3,8 +3,7 @@ package org.simplilearn.demo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World");
+		System.out.println("Not so hello world");
 	}
 
 }
